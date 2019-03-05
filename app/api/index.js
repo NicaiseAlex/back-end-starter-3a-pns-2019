c2402a305651e593d609455567ccd42838d93c91
NicaiseAlex <alexandre.nicaise@etu.univ-cotedazur.fr>
2019-03-05 09:16:04 +0100

    repush

--- app/api/index.js
+++ app/api/index.js
@@ -1,8 +1,10 @@
 const { Router } = require('express');
 const TicketRouter = require('./tickets');
+const StudentRouter = require('./student');
 
 const router = new Router();
 router.get('/status', (req, res) => res.status(200).json('ok'));
+router.use('/student', StudentRouter);
 router.use('/tickets', TicketRouter);
 
 module.exports = router;
